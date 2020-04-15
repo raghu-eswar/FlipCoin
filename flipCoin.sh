26de84f8a06e7563584c61757fdaeaa1b1b1fb26
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-15 16:58:37 +0530

    Feature added to store and to calculate percentage

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,8 +1,25 @@
 echo  "welcpome";
 
-status=$(( RANDOM % 2 ));
-if (( $status == 0)); then
-    echo "heads";
-else 
-    echo "tails";
-fi
+declare -A combinetions;
+numberOfTrails=100;
+combinetions["heads"]=0;
+combinetions["tails"]=0;
+persentageOfHeadsCombinetion=0;
+persentageOfTailsCombinetion=0;
+
+for (( counter=0; counter<$numberOfTrails; counter++))
+do
+    status=$(( RANDOM % 2 ));
+    if (( $status == 0)); then
+        echo "heads";
+        combinetions["heads"]=$(( ${combinetions[heads]} + 1));
+    else 
+        echo "tails";
+        combinetions["tails"]=$(( ${combinetions[tails]} + 1));
+    fi
+done
+
+persentageOfHeadsCombinetion=$(( $(( ${combinetions[heads]} * 100 )) / $numberOfTrails ));
+persentageOfTailsCombinetion=$(( $(( ${combinetions[tails]} * 100 )) / $numberOfTrails ));
+echo $persentageOfHeadsCombinetion;
+echo $persentageOfTailsCombinetion;
