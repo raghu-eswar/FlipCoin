d39a9d78e18e9daba02ba5a2ef88bcebcfe45254
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-04-15 19:27:04 +0530

    code refactored and feature added for all combinations

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,25 +1,49 @@
 echo  "welcpome";
 
-declare -A combinetions;
-numberOfTrails=100;
-combinetions["heads"]=0;
-combinetions["tails"]=0;
+echo "enter number of trails "; read numberOfTrails;
+echo "enter number of combinetion "; read combinetionType;
+declare -A combinetionsCount;
+combinetionsCount["heads"]=0;
+combinetionsCount["tails"]=0;
 persentageOfHeadsCombinetion=0;
 persentageOfTailsCombinetion=0;
-
+declare -A combinetionsList;
+temp=1;
 for (( counter=0; counter<$numberOfTrails; counter++))
 do
     status=$(( RANDOM % 2 ));
-    if (( $status == 0)); then
+    count=1;
+    if (( $status  == 0)); then
+        combo="heads";
+    else
+        combo="tails";
+    fi
+    tempCombo=$combo;
+    for (( i=1; i<=$combinetionType; i++ ))  
+    do
+        status2=$(( RANDOM % 2 ));
+        if (( $status == $status2 )); then
+            ((count++));
+            combo="$combo,$tempCombo";
+        else
+            break;
+        fi
+    done
+    if (( $count == $combinetionType && $status == 0)); then
         echo "heads";
-        combinetions["heads"]=$(( ${combinetions[heads]} + 1));
-    else 
+        combinetionsList[$temp]=$combo;
+        temp=$(($temp+1));
+        combinetionsCount["heads"]=$(( ${combinetionsCount[heads]} + 1));
+    elif (( $count == $combinetionType )); then
         echo "tails";
-        combinetions["tails"]=$(( ${combinetions[tails]} + 1));
+        combinetionsList[$temp]=$combo;
+        combinetionsCount["tails"]=$(( ${combinetionsCount[tails]} + 1));
+        temp=$(($temp+1));
     fi
 done
-
-persentageOfHeadsCombinetion=$(( $(( ${combinetions[heads]} * 100 )) / $numberOfTrails ));
-persentageOfTailsCombinetion=$(( $(( ${combinetions[tails]} * 100 )) / $numberOfTrails ));
+persentageOfHeadsCombinetion=$(( $(( ${combinetionsCount[heads]} * 100 )) / $numberOfTrails ));
+persentageOfTailsCombinetion=$(( $(( ${combinetionsCount[tails]} * 100 )) / $numberOfTrails ));
+# out put
 echo $persentageOfHeadsCombinetion;
 echo $persentageOfTailsCombinetion;
+echo ${combinetionsList[@]};
